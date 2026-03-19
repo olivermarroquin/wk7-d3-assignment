723536f22d8d8403933ed6d4bf4d888445e1929b
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-18 21:44:55 -0400

    committed html, css and started app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const searchForm = document.getElementById("searchForm");
+const topicInput = document.getElementById("topic");
+const toneInput = document.getElementById("tone");
+const statusText = document.getElementById("status");
+const resultText = document.getElementById("results");
+
+async function main() {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
